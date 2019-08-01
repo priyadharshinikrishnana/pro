f75ed65c3bf0522ef52040cb94a70dcc14713195
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-01 07:24:42 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,5 +1,6 @@
-sam=int(input())
-if sam%2==0:
-    print(sam*(sam//2)-(sam//2))
+# your code goes here
+samu=int(input())
+if samu%2==0:
+    print(samu*(samu//2)-(samu//2))
 else:
-    print(sam*(sam//2))
+    print(samu*(samu//2))
