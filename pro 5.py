0be080f71eeaf8b8568337bd87a6878a0d719687
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-01 08:10:50 +0530

    Create pro 5.py

---
+++ pro 5.py
@@ -0,0 +1,13 @@
+vv,pp=map(int,input().split())
+tab=[]
+cine=[]
+priya=[int(o) for o in input().split() ]
+for k in range(0,pp):
+    u1,v1=map(int,input().split())
+    for l in range(u1-1 ,v1):
+        sine.append(priya[l])
+    kah=sorted(cine)
+    tab.append(min(cine))
+    del sine[:]
+for p in range(0,len(cab)):
+    print(tab[p])
