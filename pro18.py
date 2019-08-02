2157adee44054e8caa07e6b2663743a3fe58c547
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 19:49:47 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,16 +1,16 @@
 priya,nivi=map(int,input().split())
-log=[]
+moon=[]
 soo=0
 for i in range(priya):
-    sak.append(list(map(int,input().split())))   
+    moon.append(list(map(int,input().split())))   
 for i in range(priya):
     for j in range(nivi-1):
         for k in range(j+1,nivi+1):
-            if log[i][j:k]==[1]*len(log[i][j:k]):
-                 if all(log[p+i][j:k]==[1]*len(log[p+i][j:k]) for p in range(len(log[i][j:k])-1)):
-                     if len(log[i][j:k])>tel:
-                        soo=len(log[i][j:k])
-if len(log)==1 and len(log[0])==1 and log[0][0]==1:
+            if moon[i][j:k]==[1]*len(moon[i][j:k]):
+                 if all(moon[p+i][j:k]==[1]*len(moon[p+i][j:k]) for p in range(len(moon[i][j:k])-1)):
+                     if len(moon[i][j:k])>tel:
+                        soo=len(moon[i][j:k])
+if len(moon)==1 and len(moon[0])==1 and moon[0][0]==1:
     print(1)
 for i in range(soo):
     print(*[1]*soo)
