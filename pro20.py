533ac8c72eb1be2078e72ce7be7e477703e03e0f
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-03 00:12:04 +0530

    Update pro20.py

--- pro20.py
+++ pro20.py
@@ -1,17 +1,17 @@
-priya,viji=(map(int,input().split())
+priya,viji=map(int,input().split())
 msd=list(map(int,input().split()))
-msd.sort()
-msd.reverse()
+asd.sort()
+asd.reverse()
 a=viji
-poo=0
+ooo=0
 for i in msd:
     if(a>=i):
-        sad=int(a/i)
-        poo=poo+sad
-        a=a-sad*i
+        any=int(a/i)
+        ooo=ooo+any
+        a=a-any*i
     if a==0:
         break
 if(a==0):
-   print(poo)
+   print(ooo)
 else:
    print("it's not posiible to select coins in such a way that they sum upto",viji)
