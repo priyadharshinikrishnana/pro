4744db82415bb665460e5820a3c49844f7236857
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 20:10:52 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,6 +1,6 @@
 priya,nivi=map(int,input().split())
 moon=[]
-soo=0
+con=0
 for i in range(priya):
     moon.append(list(map(int,input().split())))   
 for i in range(priya):
@@ -8,9 +8,9 @@ for i in range(priya):
         for k in range(j+1,nivi+1):
             if moon[i][j:k]==[1]*len(moon[i][j:k]):
                  if all(moon[p+i][j:k]==[1]*len(moon[p+i][j:k]) for p in range(len(moon[i][j:k])-1)):
-                     if len(moon[i][j:k])>tel:
-                        soo=len(moon[i][j:k])
+                     if len(moon[i][j:k])>moon:
+                        con=len(moon[i][j:k])
 if len(moon)==1 and len(moon[0])==1 and moon[0][0]==1:
     print(1)
-for i in range(soo):
-    print(*[1]*soo)
+for i in range(con):
+    print(*[1]*con)
