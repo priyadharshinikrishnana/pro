71501233c5450bf52c1a747bb63779d43572d4f4
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 19:33:09 +0530

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,9 +1,9 @@
-priya,sen = input().split()
-nivi = int(nivi)
+priya,roja = input().split()
+roja = int(roja)
 rome = False
 see = list(map(int,input().split()))
 for i in range(len(see)):
     for j in range(len(see)):
-        if see[i]+see[j] == nivi:
+        if see[i]+see[j] == roja:
             room = True
 print("yes" if room else "no")
