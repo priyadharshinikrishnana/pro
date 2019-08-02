ace4c124b96d14643982cec2f05fa35536c66f8e
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 15:10:34 +0530

    Update pro13.py

--- pro13.py
+++ pro13.py
@@ -1,13 +1,13 @@
-nn,mm=map(int,input().split())
-chlm=[]
-vinn=[]
-priya=[int(m) for m in input().split() ]
-for i in range(0,mm):
+p,q=map(int,input().split())
+ab=[]
+stu=[]
+viji=[int(n) for n in input().split() ]
+for i in range(0,q):
     a1,b1=map(int,input().split())
     for j in range(a1-1 ,b1):
-        vinn.append(priya[j])
-    mah=sorted(vinn)
-    qwe.append(min(vinn))
-    del vinn[:]
-for z in range(0,len(qwe)):
-    print(qwe[z])
+        stu.append(viji[j])
+    pri=sorted(stu)
+    ab.append(min(stu))
+    del stu[:]
+for z in range(0,len(ab)):
+    print(ab[z])
