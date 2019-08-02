1d0c937c8cdb3f5d67a812b9bfb233511316d561
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 15:01:45 +0530

    Update pro13.py

--- pro13.py
+++ pro13.py
@@ -1,13 +1,13 @@
 nn,mm=map(int,input().split())
 chlm=[]
-coss=[]
+vinn=[]
 priya=[int(m) for m in input().split() ]
 for i in range(0,mm):
     a1,b1=map(int,input().split())
     for j in range(a1-1 ,b1):
-        sine.append(priya[j])
-    mah=sorted(coss)
-    qwe.append(min(coss))
-    del coss[:]
+        vinn.append(priya[j])
+    mah=sorted(vinn)
+    qwe.append(min(vinn))
+    del vinn[:]
 for z in range(0,len(qwe)):
     print(qwe[z])
