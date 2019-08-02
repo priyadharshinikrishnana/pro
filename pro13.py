d7aec984d9175fac2aeec91d4f4068963978bd8f
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-02 14:55:07 +0530

    Create pro13.py

---
+++ pro13.py
@@ -0,0 +1,13 @@
+nn,mm=map(int,input().split())
+chlm=[]
+coss=[]
+priya=[int(m) for m in input().split() ]
+for i in range(0,mm):
+    a1,b1=map(int,input().split())
+    for j in range(a1-1 ,b1):
+        sine.append(priya[j])
+    mah=sorted(coss)
+    qwe.append(min(coss))
+    del coss[:]
+for z in range(0,len(qwe)):
+    print(qwe[z])
