80798e74802501e63cc689b5fb75dd744bfb7df3
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-05 14:38:19 +0530

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,9 +1,9 @@
-priyaa=int(input())
+ammu=int(input())
 vpv=input().split()
 ks=[]
-for i in range(priya):
+for i in range(ammu):
     vijii=vpv[i]
-    for j in range(i+1,priya):
+    for j in range(i+1,ammu):
         if(int(vpv[i])<int(vpv[j]))and (int(vpv[j-1])<int(vpv[j])):
             vijii+=vpv[j]
         else:
