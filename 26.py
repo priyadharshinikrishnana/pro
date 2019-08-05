0fdd7fd3dd483735293d8a32fa73080756403973
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-05 15:33:49 +0530

    Update 26.py

--- 26.py
+++ 26.py
@@ -1,14 +1,13 @@
-    
-priyaa = int(input())
-viji = list(map(int, input().split()))
+nfg = int(input())
+lak = list(map(int, input().split()))
 maximum = 0
-eee = 0
-any = viji[0]
-for i in range(0,priya-1):
-    if any < viji[i+1]:
-        eee +=1
-        any = viji[i+1]
+coc = 0
+any = lak[0]
+for i in range(0,nfg-1):
+    if any < lak[i+1]:
+        coc +=1
+        any = lak[i+1]
     else:
-        if max(viji[i+1:]) < any:
-            any = viji[i+1]
-print(eee+1)
+        if max(lak[i+1:]) < any:
+            any = lak[i+1]
+print(coc+1)
