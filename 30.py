d205d50266d2bc9adfc75f474ea1e79818bb4c92
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-06 00:13:25 +0530

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,11 +1,12 @@
-vpo=(input())
-nnn=0
-for i in range(0,len(vpo)):
-   fff=(vpo[:i]+vpo[i+1:])
-    if(int(fff) % 8==0):
-        nnn=1
+#vsb
+hoo=(input())
+rat=0
+for i in range(0,len(hoo)):
+    ppp=(hoo[:i]+hoo[i+1:])
+    if(int(ppp) % 8==0):
+       rat=1
         break
-if(nnn==1):
+if(rat==1):
     print("yes")
 else:
     print("no")
