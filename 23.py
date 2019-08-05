51b49ef2808474148795e49974537a88d5caca7e
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-05 14:10:13 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,12 +1,5 @@
-priya=int(input())
-vijii=2**priya
-a=[]
-for i in range(0,vijii):
-    u=bin(i)[2:].afill(priya)
-    if(len(u)<len(bin(2**priya-1)[2:])):
-        a.append([u.count("1"),u])
-    else:
-        a.append([u.count("1"),u])
-a.sort()
-for i in range(len(a)):
-    print(a[i][1])
+priya=input()
+if priya.find("GRGRGR")!=-1 or priya.find("GLGLGL")!=-1 or priya.find("GLLG")!=-1 or priya.find("GRRG")!=-1:
+  print("yes")
+else:
+  print("no")
