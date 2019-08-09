c01ba5022b594fa0606c8908cfefe8a97e3f143e
priyadharshinikrishnana <52317670+priyadharshinikrishnana@users.noreply.github.com>
2019-08-09 14:42:02 +0530

    Update 39.py

--- 39.py
+++ 39.py
@@ -1,11 +1,11 @@
-priya,l1=map(str,input().split())
+priya,l2=map(str,input().split())
 viji=0
-if len(priya)>len(l1):
-  priya,l1=l1,priya
-pt1=0
-while pt1<len(priya):
-  viji+=(ord(l1[pt1])-ord(vizh[pt1]))
-  pt1+=1
-for pt1 in range(pt1,len(l1)):
-  viji+=ord(l1[pt1])-ord('a')+1
+if len(priya)>len(l2):
+  priya,l2=l2,priya
+pt2=0
+while pt2<len(priya):
+  viji+=(ord(l2[pt2])-ord(vizh[pt2]))
+  pt2+=1
+for pt2 in range(pt2,len(l2)):
+  viji+=ord(l2[pt2])-ord('a')+1
 print(viji)
